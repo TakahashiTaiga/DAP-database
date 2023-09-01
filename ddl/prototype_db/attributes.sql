e92b15de68aad24182a89ede662cf619a7027f34
Takahashi Taiga <beambitioustiger@gmail.com>
2023-09-01 11:08:33 +0900

    fix sql

--- ddl/prototype_db/attributes.sql
+++ ddl/prototype_db/attributes.sql
@@ -1,4 +1,4 @@
-CREATE TABLE attribute (
+CREATE TABLE attributes (
     attribute_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     attribute_set_id INT UNSIGNED NOT NULL,
     attribute_name VARCHAR(256) NOT NULL,
