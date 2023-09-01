6f82fa3463805180e4ea58641923f99ac1760c92
Takahashi Taiga <beambitioustiger@gmail.com>
2023-09-01 10:47:31 +0900

    fix sql

--- ddl/prototype_db/users.sql
+++ ddl/prototype_db/users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
-    user_id int UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    user_name varchar(265) NOT NULL,
-    email_address varchar(265) NOT NULL,
-    user_password varchar(1024) NOT NULL
+    user_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    user_name VARCHAR(256) NOT NULL,
+    email_address VARCHAR(256) NOT NULL,
+    user_password VARCHAR(1024) NOT NULL
 );
