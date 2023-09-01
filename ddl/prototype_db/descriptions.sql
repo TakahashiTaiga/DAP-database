d11d6f750c3d8a6ec6f1162fae0eaffbeb981466
Takahashi Taiga <beambitioustiger@gmail.com>
2023-09-01 11:34:55 +0900

    add descriptions.sql

---
+++ ddl/prototype_db/descriptions.sql
@@ -0,0 +1,8 @@
+CREATE TABLE descriptions (
+    description_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    file_id INT UNSIGNED NOT NULL,
+    attribute_id INT UNSIGNED NOT NULL,
+    content VARCHAR(2048),
+    FOREIGN KEY file_id(file_id) REFERENCES files(file_id),
+    FOREIGN KEY attribute_id(attribute_id) REFERENCES attributes(attribute_id)
+);
