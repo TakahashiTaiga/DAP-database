e877860d0337c73f8f3c523ab2b64146e75482f0
Takahashi Taiga <beambitioustiger@gmail.com>
2023-09-01 11:25:53 +0900

    files.sql

--- ddl/prototype_db/files.sql
+++ ddl/prototype_db/files.sql
@@ -1,6 +1,6 @@
 CREATE TABLE files (
     file_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     lowest_level_category_id INT UNSIGNED NOT NULL,
-    file_name TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    file_name VARCHAR(1024) DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY lowest_level_category_id(lowest_level_category_id) REFERENCES categories(category_id)
 );
