016f209fbc85516b295dcd71f1e5cd0c9a3e83a1
Takahashi Taiga <beambitioustiger@gmail.com>
2023-09-01 11:02:18 +0900

    add attributes.sql

---
+++ ddl/prototype_db/attributes.sql
@@ -0,0 +1,6 @@
+CREATE TABLE attribute (
+    attribute_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    attribute_set_id INT UNSIGNED NOT NULL,
+    attribute_name VARCHAR(256) NOT NULL,
+    FOREIGN KEY attribute_set_id(attribute_set_id) REFERENCES attribute_sets(attribute_set_id)
+);
