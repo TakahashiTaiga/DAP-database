49daf9e193e96e284f0fbc98338d2dc3834bdf51
Takahashi Taiga <beambitioustiger@gmail.com>
2023-09-05 21:35:02 +0900

    add file_path in files.sql

--- ddl/prototype_db/files.sql
+++ ddl/prototype_db/files.sql
@@ -2,5 +2,6 @@ CREATE TABLE files (
     file_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     lowest_level_category_id INT UNSIGNED NOT NULL,
     file_name VARCHAR(1024) NOT NULL,
+    file_path VARCHAR(1024) NOT NULL,
     FOREIGN KEY lowest_level_category_id(lowest_level_category_id) REFERENCES categories(category_id)
 );
